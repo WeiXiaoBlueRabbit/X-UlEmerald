f48fe7e1de4260b845a3382c9d4ea7515f8f71e2
WeiXiaoBlueRabbit <53326786+WeiXiaoBlueRabbit@users.noreply.github.com>
2021-07-31 18:05:20 +0800

    Update

--- src/frame/GamePanel.java
+++ src/frame/GamePanel.java
@@ -10,7 +10,6 @@ public class GamePanel extends JPanel {
 
     IDrawImage[] draws;
     private Image image;//缓冲
-    private Image image_1 = Common.getImage("map_1.jpg");//背景图片
 
     public GamePanel(IDrawImage... draws) {
         this.draws = draws;
@@ -21,7 +20,6 @@ public class GamePanel extends JPanel {
     private void drawBufferedImage() {
         image = createImage(this.getWidth(), this.getHeight());
         Graphics g = image.getGraphics();
-        g.drawImage(image_1, 0, 0, this.getWidth(), this.getHeight(), this);
         //绘制
         for (IDrawImage draw : this.draws) {
             draw.drawImage(g);
